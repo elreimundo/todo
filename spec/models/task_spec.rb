a71fcbae2ae08be5ac94087f418b6e6788611495
David Reiman <david.reiman@gmail.com>
2013-12-20 00:01:21 -0800

    reworked test to conform to Rails standards

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Task do
   context "#initialize" do
   	it "should initialize with one title argument" do
-			expect{Task.new("Make this test pass")}.not_to raise_error
+			expect{Task.new(title: "Make this test pass")}.not_to raise_error
 	  end
   end
 end
