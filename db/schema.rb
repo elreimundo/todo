6fdd88ffc6f31153489bf4c5bb07473fc14f7680
David Reiman <david.reiman@gmail.com>
2013-12-20 07:00:34 -0800

    migrate change to completed column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131220081451) do
+ActiveRecord::Schema.define(version: 20131220145636) do
 
   create_table "tasks", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
-    t.boolean  "completed"
+    t.boolean  "completed",  default: false
   end
 
 end
