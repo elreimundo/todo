6c6b70b8a01eb5b89b38b6d56548833bf508c175
David Reiman <david.reiman@gmail.com>
2013-12-20 00:04:03 -0800

    next failing test to validate presence of title

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Task do
   context "#initialize" do
+  	it {should validate_presence_of :title}
   	it "should initialize with one title argument" do
 			expect{Task.new(title: "Make this test pass")}.not_to raise_error
 	  end
